a3b97f498ecb7e4770ce7ea31a5fab9c373f8efd
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-13 21:08:52 +0000

    add social media and edits to navbar

--- src/js/component/Nav.js
+++ src/js/component/Nav.js
@@ -44,19 +44,12 @@ export default function Nav() {
 							</a>
 						</li>
 					</ul>
-					<form className="form-inline mt-2 mt-md-0">
-						<input
-							className="form-control mr-sm-2"
-							type="text"
-							placeholder="Search"
-							aria-label="Search"
-						/>
-						<button
-							className="btn btn-outline-success my-2 my-sm-0"
-							type="submit">
-							Search
-						</button>
-					</form>
+					<div className="socialMediaLinks">
+						<i className="fab fa-instagram" />
+						<i className="fab fa-twitter" />
+						<i className="fab fa-facebook" />
+						<i className="fas fa-share-alt" />
+					</div>
 				</div>
 			</nav>
 		</div>

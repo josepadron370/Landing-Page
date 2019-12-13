0c4e9135fac6142bf3f805621ec700c87f8aa41e
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-13 20:46:30 +0000

    remove background image

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -8,7 +8,7 @@ export default function Jumbotron() {
 	return (
 		<div className="text-center mt-5">
 			Jumbotron
-			<div className="jumbotron jumbotron-fluid">
+			<div className="jumbotron1 jumbotron-fluid">
 				<div className="container transbox mx-auto">
 					<h1 className="display-4">Fluid jumbotron</h1>
 					<p className="lead">

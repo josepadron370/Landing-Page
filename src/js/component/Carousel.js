abaa12330ac2d53c4c9eefe7b6c6b06cc228e1ea
afrayz29 <alfrayman@gmail.com>
2019-12-13 21:54:46 +0000

    update

--- src/js/component/Carousel.js
+++ src/js/component/Carousel.js
@@ -1,17 +1,65 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import christmanTime from "../../img/christmas1.jpg";
+import christmanTime1 from "../../img/christmas2.jpg";
 
 //create your first component
 export default function Carousel() {
 	return (
-		<div className="carousel-item">
-			<img src="..." alt="..." />
-			<div className="carousel-caption d-none d-md-block">
-				<h5>...</h5>
-				<p>...</p>
+		<div
+			id="carouselExampleIndicators"
+			className="carousel slide"
+			data-ride="carousel">
+			<ol className="carousel-indicators">
+				<li
+					data-target="#carouselExampleIndicators"
+					data-slide-to={0}
+					className="active"
+				/>
+				<li
+					data-target="#carouselExampleIndicators"
+					data-slide-to={1}
+				/>
+			</ol>
+			<div className="carousel-inner">
+				<div className="carousel-item active">
+					<img
+						className="d-block w-100"
+						src={christmanTime}
+						alt="First slide"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						className="d-block w-100"
+						src={christmanTime1}
+						alt="Second slide"
+					/>
+				</div>
 			</div>
+			<a
+				className="carousel-control-prev"
+				href="#carouselExampleIndicators"
+				role="button"
+				data-slide="prev">
+				<span
+					className="carousel-control-prev-icon"
+					aria-hidden="true"
+				/>
+				<span className="sr-only">Previous</span>
+			</a>
+			<a
+				className="carousel-control-next"
+				href="#carouselExampleIndicators"
+				role="button"
+				data-slide="next">
+				<span
+					className="carousel-control-next-icon"
+					aria-hidden="true"
+				/>
+				<span className="sr-only">Next</span>
+			</a>
 		</div>
 	);
 }

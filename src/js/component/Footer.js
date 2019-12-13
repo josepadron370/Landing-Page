ee96912a9994734abb7fc6dcab8d4b57b44748f5
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-13 19:59:33 +0000

    Nav-Jumbotron-Footer-first-commit

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -5,5 +5,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export default function Footer() {
-	return <div className="text-center mt-5">Footer</div>;
+	return (
+		<div className="text-center mt-5">
+			Footer
+			<footer className="footer mt-auto py-3">
+				<div className="container">
+					<span className="text-muted">
+						Place sticky footer content here.
+					</span>
+				</div>
+			</footer>
+		</div>
+	);
 }

f10eb05e963f8d6167fb06ad6ab3074ca1a4aeb1
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-16 16:53:26 +0000

    add video carousel componnent

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -16,7 +16,7 @@ export function Home() {
 	return (
 		<>
 			<Nav />
-			{/* <Image /> */}
+			<Image />
 			<Jumbotron />
 			<CardDeck />
 			<Carousel />
--- src/js/component/Image.js
+++ src/js/component/Image.js
@@ -1,35 +1,53 @@
-// import React from "react";
+import React from "react";
 
-// //include images into your bundle
-// import rigoImage from "../../img/rigo-baby.jpg";
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
-// //create your first component
-// export default function Image() {
-// return (
-// <>
-// <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
-//   <div className="carousel-inner">
-//     <div className="carousel-item active" data-interval={10000}>
-//         <video width="320" height="240" controls>
-//             <source src="movie.mp4" type="video/mp4">
-//             <source src="movie.ogg" type="video/ogg">
-//             Your browser does not support the video tag.
-//        </video>
-//    </div>
-//      <div className="carousel-item" data-interval={2000}>
-//         <video width="320" height="240" controls>
-//             <source src="movie.mp4" type="video/mp4">
-//             <source src="movie.ogg" type="video/ogg">
-//             Your browser does not support the video tag.
-//        </video>
-//      </div>
-//       <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-//          <span className="carousel-control-prev-icon" aria-hidden="true" />
-//          <span className="sr-only">Previous</span>
-//      </a>
-//     <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-//          <span className="carousel-control-next-icon" aria-hidden="true" />
-//          <span className="sr-only">Next</span>
-//      </a>
-//  </div>
-// </>);
+//create your first component
+export default function Image() {
+	return (
+		<div
+			id="carouselExampleInterval"
+			className="carousel slide"
+			data-ride="carousel">
+			<div className="carousel-inner">
+				<div className="carousel-item active" data-interval={10000}>
+					<video width="100%" height="auto" controls>
+						<source src="movie.mp4" type="video/mp4" />
+						<source src="movie.ogg" type="video/ogg" />
+						Your browser does not support the video tag.
+					</video>
+				</div>
+				<div className="carousel-item" data-interval={2000}>
+					<video width="100%" height="auto" controls>
+						<source src="movie.mp4" type="video/mp4" />
+						<source src="movie.ogg" type="video/ogg" />
+						Your browser does not support the video tag.
+					</video>
+				</div>
+				<a
+					className="carousel-control-prev"
+					href="#carouselExampleInterval"
+					role="button"
+					data-slide="prev ">
+					<span
+						className="carousel-control-prev-icon"
+						aria-hidden="true"
+					/>
+					<span className="sr-only">Previous</span>
+				</a>
+				<a
+					className="carousel-control-next"
+					href="#carouselExampleInterval"
+					role="button"
+					data-slide="next">
+					<span
+						className="carousel-control-next-icon"
+						aria-hidden="true"
+					/>
+					<span className="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+	);
+}

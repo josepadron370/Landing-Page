bf8b347965ba2f08c5cd4f085242a2a13e7ec2b9
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-16 16:25:55 +0000

    add image.js for video carousel

--- src/js/component/Image.js
+++ src/js/component/Image.js
@@ -5,27 +5,31 @@
 
 // //create your first component
 // export default function Image() {
-// 	return (
-//         <>
-//         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
-//           <div className="carousel-inner">
-//             <div className="carousel-item active" data-interval={10000}>
-//                 <img src="..." className="d-block w-100" alt="..." />
-//             </div>
-//             <div className="carousel-item" data-interval={2000}>
-//                 <img src="..." className="d-block w-100" alt="..." />
-//             </div>
-//             <div className="carousel-item">
-//                 <img src="..." className="d-block w-100" alt="..." />
-//             </div>
-//           </div>
-//             <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-//                 <span className="carousel-control-prev-icon" aria-hidden="true" />
-//                 <span className="sr-only">Previous</span>
-//             </a>
-//             <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-//                 <span className="carousel-control-next-icon" aria-hidden="true" />
-//                 <span className="sr-only">Next</span>
-//             </a>
-//         </div>
-//         </>);
+// return (
+// <>
+// <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+//   <div className="carousel-inner">
+//     <div className="carousel-item active" data-interval={10000}>
+//         <video width="320" height="240" controls>
+//             <source src="movie.mp4" type="video/mp4">
+//             <source src="movie.ogg" type="video/ogg">
+//             Your browser does not support the video tag.
+//        </video>
+//    </div>
+//      <div className="carousel-item" data-interval={2000}>
+//         <video width="320" height="240" controls>
+//             <source src="movie.mp4" type="video/mp4">
+//             <source src="movie.ogg" type="video/ogg">
+//             Your browser does not support the video tag.
+//        </video>
+//      </div>
+//       <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+//          <span className="carousel-control-prev-icon" aria-hidden="true" />
+//          <span className="sr-only">Previous</span>
+//      </a>
+//     <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+//          <span className="carousel-control-next-icon" aria-hidden="true" />
+//          <span className="sr-only">Next</span>
+//      </a>
+//  </div>
+// </>);
